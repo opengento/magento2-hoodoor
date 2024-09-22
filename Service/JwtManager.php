ab7995055ba9b99b3f33a0bd5bf109ef0b27233b
Ronan Guérin <guerin.ronan@proton.me>
2024-09-22 10:54:28 +0200

    add copyright

--- Service/JwtManager.php
+++ Service/JwtManager.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * Copyright © OpenGento, All rights reserved.
+ * See LICENSE bundled with this library for license details.
+ */
 declare(strict_types=1);
 
 namespace Opengento\Hoodoor\Service;
